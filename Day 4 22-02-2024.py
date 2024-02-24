9249df8864c0e9b2e8d7645f0a5678387af47cc6
aishu1643 <160564585+aishu1643@users.noreply.github.com>
2024-02-24 10:46:23 +0530

    Update Day 4 22-02-2024.py

--- Day 4 22-02-2024.py
+++ Day 4 22-02-2024.py
@@ -102,3 +102,19 @@ if city in route:
     print(*route[city],sep=" ")
 else:
     print("None")
+#P7
+#Approach - 1
+n = int(input())
+route = {}
+for i in range(n):
+    c1,c2 = input().split()
+    if c1 not in route:
+        route[c1] = [c2]
+    else:
+        route[c1].append(c2)
+print(route)
+city = input()
+if city in route:
+    print(*route[city],sep=" ")
+else:
+    print("None")
