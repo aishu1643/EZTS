3c0d10316c7a7ceb070ae2dda1cb65ca108ba3da
aishu1643 <160564585+aishu1643@users.noreply.github.com>
2024-02-24 10:22:16 +0530

    Update Day 5 23-02-2024.py

--- Day 5 23-02-2024.py
+++ Day 5 23-02-2024.py
@@ -234,3 +234,54 @@ for i in range(t):
         else:
             r=r+chr(k)
 print(r)
+#OOPS
+#P11
+class cse:
+    def hello(self):
+        print("Hello cse")
+class aiml:
+    def hello(self):
+        print("Hello aiml")
+ob1=cse()
+ob1.hello()
+#P12
+class classa:
+    def factorial(self,n):
+        r = 1
+        for i in range(1,n+1):
+            r = r * i
+        print(r)
+ob = classa()
+ob.factorial(5)
+#P13
+class a:
+    def __init__(self):
+        print("Hello")
+    def hello(self):
+        print("Hello a")
+ob = a()
+#P14
+class classa:
+    def __init__(self,n):
+        self.n = n
+        print(n)
+    def factorial(self):
+        r = 1
+        for i in range(1,self.n+1):
+            r = r * i
+        print(r)
+ob = classa(5)
+ob.factorial()
+#P15
+class classa:
+    def __init__(self,n):
+        self.n = n
+    def factorial(self):
+        print(self.fact(self.n))
+    def fact(self,n):
+        if n == 1:
+            return 1
+        else:
+            return n * self.fact(n-1)
+ob = classa(5)
+ob.factorial()
